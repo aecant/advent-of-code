89682bd94581d20c1f87519c0fec780540fde004
Alessandro Cantini <aecantini@gmail.com>
2020-12-05 13:11:17 +0100

    Add day 5

---
+++ 2020/day5/solution.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+def bin_search(sequence, first_half_char):
+    left = 0
+    right = 2**len(sequence)
+
+    for char in sequence:
+        if char == first_half_char:
+            right -= (right - left) // 2
+        else:
+            left += (right - left) // 2
+
+    return left
+
+
+def get_seat_id(ticket):
+    row = bin_search(ticket[:-3], 'F')
+    col = bin_search(ticket[-3:], 'L')
+    return row * 8 + col
+
+
+def get_my_seat_id(seat_ids):
+    for seat_id, next_seat_id in zip(seat_ids, seat_ids[1:]):
+        if seat_id + 1 != next_seat_id:
+            return seat_id + 1
+
+
+lines = Path('input.txt').read_text().splitlines()
+seat_ids = sorted(map(get_seat_id, lines))
+
+part1_result = seat_ids[-1]
+part2_result = get_my_seat_id(seat_ids)
+
+print('part1:', part1_result)
+print('part2:', part2_result)
