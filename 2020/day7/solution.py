82b13637bab6a56823cdb630d90f0215b08b941f
Alessandro Cantini <aecantini@gmail.com>
2020-12-07 18:27:01 +0100

    Add day 7

---
+++ 2020/day7/solution.py
@@ -0,0 +1,49 @@
+import re
+from pathlib import Path
+from collections import defaultdict
+
+
+def parse_rule(s):
+    parent = ' '.join(s.split()[:2])
+    children = [(int(qty), child) for qty, child in re.findall(r'(\d+) (\w+ \w+) bags?[,.]', s)]
+    return parent, children
+
+
+def get_parents(children):
+    dic = defaultdict(list)
+    for parent, children in children.items():
+        for _, child in children:
+            dic[child].append(parent)
+
+    return dic
+
+
+def count_possible_parents(bag, parents_dict):
+    queue = parents_dict[bag].copy()
+    counted = set()
+    while queue:
+        bag = queue.pop()
+        if bag not in counted:
+            queue.extend(parents_dict[bag])
+            counted.add(bag)
+
+    return len(counted)
+
+
+def count_required_children(bag, children, cache={}):
+    if not children[bag]:
+        return 1
+    if bag not in cache:
+        cache[bag] = 1 + sum(count_required_children(child, children, cache) * qty for qty, child in children[bag])
+
+    return cache[bag]
+
+
+lines = Path('input.txt').read_text().splitlines()
+
+children = dict(map(parse_rule, lines))
+parents = get_parents(children)
+
+print('part1:', count_possible_parents('shiny gold', parents))
+print('part2:', count_required_children('shiny gold', children) - 1)
+
