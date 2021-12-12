ac8b5c40958f332069ce3c8dba6a35491cc25381
Alessandro Elio Cantini <aecantini@gmail.com>
2021-12-12 15:59:21 +0100

    add 2021 day3 part 2

--- 2021/day3/solution.py
+++ 2021/day3/solution.py
@@ -1,4 +1,7 @@
+import itertools
+import operator
 from pathlib import Path
+
 import numpy as np
 
 
@@ -15,14 +18,30 @@ def bin_array_to_int(arr):
 def solve_part1(matrix):
     columns_sum = matrix.sum(axis=0)
     num_rows = matrix.shape[0]
-    gamma_bool_array = columns_sum > num_rows / 2
-    gamma_int_array = gamma_bool_array.astype(int)
-    gamma = bin_array_to_int(gamma_int_array)
-    epsilon = bin_array_to_int(gamma_int_array ^ 1)
+    most_common_bits = columns_sum > num_rows / 2
+    gamma = bin_array_to_int(most_common_bits.astype(int))
+    epsilon = bin_array_to_int(most_common_bits ^ 1)
     return gamma * epsilon
 
-    
+
+def find_rating(matrix, op):
+    matrix = matrix.copy()
+    for col_idx in itertools.count():
+        num_rows = matrix.shape[0]
+        col = matrix[:, col_idx]
+        bit_criteria = op(col.sum(), num_rows / 2)
+        matrix = np.array([row for row in matrix if row[col_idx] == bit_criteria])
+        if len(matrix) == 1:
+            return bin_array_to_int(matrix[0])
+
+
+def solve_part2(matrix):
+    oxygen = find_rating(matrix, operator.ge)
+    co2 = find_rating(matrix, operator.lt)
+    return oxygen * co2
 
 
 matrix = parse_matrix('input.txt')
-print(solve_part1(matrix))
+
+assert solve_part1(matrix) == 1092896
+assert solve_part2(matrix) == 4672151
