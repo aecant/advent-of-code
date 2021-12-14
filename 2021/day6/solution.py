635da41ac3fd701fee8f4daf0f169fdbf35e5805
Alessandro Elio Cantini <aecantini@gmail.com>
2021-12-14 22:21:34 +0100

    2021 day6 part2

--- 2021/day6/solution.py
+++ 2021/day6/solution.py
@@ -1,23 +1,24 @@
 from pathlib import Path
+from functools import cache
 
 
 def parse_lanternfishes(filename):
     return [int(val) for val in Path(filename).read_text().split(',')]
 
 
-def find_lanternfishes(initial_lanternfishes, days):
-    lfishes = initial_lanternfishes.copy()
-    for day in range(days):
-        print(day, len(lfishes))
-        for i, lfish in enumerate(lfishes):
-            if lfish == 0:
-                lfishes[i] = 6
-                lfishes.append(9)
-            else:
-                lfishes[i] -= 1
-    return lfishes
+@cache
+def count_children(days_to_first_child, days_left):
+    return sum(
+        1 + count_children(9, days_left - day)
+        for day in range(days_to_first_child, days_left, 7)
+    )
 
 
-init_lfishes = parse_lanternfishes('test_input.txt')
-init_lfishes = [1]
-print(len(find_lanternfishes(init_lfishes, 100)))
+def count_lanternfishes(initial, days):
+    return len(initial) + sum(count_children(lfish, days) for lfish in initial)
+
+
+init_lfishes = parse_lanternfishes('input.txt')
+
+assert count_lanternfishes(init_lfishes, 80) == 396210
+assert count_lanternfishes(init_lfishes, 256) == 1770823541496
